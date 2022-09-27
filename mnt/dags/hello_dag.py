a84f2b57f876813514316c24b8a7be9067a860cd
atbee <athibet.pi@gmail.com>
2022-09-28 00:55:29 +0700

    Create the hello dag :bee:

---
+++ mnt/dags/hello_dag.py
@@ -0,0 +1,28 @@
+from airflow import DAG
+from airflow.utils import timezone
+from airflow.operators.bash import BashOperator
+from airflow.operators.empty import EmptyOperator
+
+
+default_args = {
+    "owner": "atb",
+}
+
+
+with DAG(
+    "hello_dag",
+    default_args=default_args,
+    schedule_interval=None,
+    start_date=timezone.datetime(2022, 9, 28),
+    catchup=False,
+) as dag:
+    start = EmptyOperator(task_id="start")
+
+    say_hello = BashOperator(
+        task_id="say_hello",
+        bash_command="echo hello",
+    )
+
+    end = EmptyOperator(task_id="end")
+
+    start >> say_hello >> end
